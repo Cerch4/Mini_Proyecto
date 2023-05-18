6e13baa6b256d5f84a1c218d4cc705571a18aa2a
Cerch4 <114672605+Cerch4@users.noreply.github.com>
2023-05-18 03:53:33 -0400

    Add files via upload
    Actualización final del mini proyecto

--- Mini Proyecto/main.cpp
+++ Mini Proyecto/main.cpp
@@ -20,26 +20,28 @@ test.print();
 */
 
 srand(time(NULL)); 
-  ofstream outfile("EXP_ArrSize64_100kElement.csv");
-  outfile<<"n-time[ms]\n";
-  int N = 200, n_1 = 1, k = 10, num_experiments =5000;
+  ofstream outfile("resultsListArr_left_10k.csv");
+  outfile<<"n,time[ms]\n";
+  int N = 200, n_1 = 1, k = 10, num_experiments = 10000;
+
   ListArr Test(64);
- 
 
   for(int n = n_1; n <= N; n += k){
+     // ListArr Test(64);
+      //Test.insert_left(10);
+
       double total_time = 0;
       for(int i = 0; i < num_experiments; i++){
         auto start_time = chrono::high_resolution_clock::now();
-        Test.insert_left(i);
+          Test.insert_left(i);
         auto end_time = chrono::high_resolution_clock::now();
         auto single_execution_time = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
           total_time += single_execution_time;
       }
-      double avg_time = total_time/num_experiments; // num_experiments;
-      outfile<<n<<" - "<<avg_time<<endl;
-
+      double avg_time = total_time / num_experiments;
+      outfile<<n<<" ,"<<avg_time<<endl;
   }
-
   outfile.close(); 
-  return(0);
+
+  return 0;
 }
